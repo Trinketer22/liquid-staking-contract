fec5c92fdd412facfe56106e8a67b9ea8f6c5be5
Trinketer22 <trinketer22@localhost>
2025-04-04 23:55:07 +0300

    Tolk conversion

--- wrappers/Controller.compile.ts
+++ wrappers/Controller.compile.ts
@@ -3,7 +3,7 @@ import { readFile, writeFile, mkdir } from 'fs/promises';
 import path from 'path';
 
 export const compile: CompilerConfig = {
-    lang: 'func',
+    lang: 'tolk',
     preCompileHook: async () => {
         const path_to_head = (await readFile(path.join(__dirname, '..', '.git', 'HEAD'), "utf-8")).split(": ")[1].trim();
         const head = await readFile(path.join(__dirname, '..', '.git', path_to_head), "utf-8");
@@ -12,6 +12,5 @@ export const compile: CompilerConfig = {
         await mkdir(auto, { recursive: true });
         await writeFile(path.join(auto, 'git-hash.func'), `const int git_hash = 0x${head.trim()};`);
     },
-    targets: ['contracts/versioning.func',
-              'contracts/controller.func'],
+    entrypoint: 'contracts/controller.tolk',
 };
--- wrappers/Pool.compile.ts
+++ wrappers/Pool.compile.ts
@@ -4,22 +4,21 @@ import path from 'path';
 import { Metadata } from "../PoolConstants";
 
 export const compile: CompilerConfig = {
-    lang: 'func',
+    lang: 'tolk',
     preCompileHook: async () => {
         const path_to_head = (await readFile(path.join(__dirname, '..', '.git', 'HEAD'), "utf-8")).split(": ")[1].trim();
         const head = await readFile(path.join(__dirname, '..', '.git', path_to_head), "utf-8");
 
         const auto = path.join(__dirname, '..', 'contracts', 'auto');
         await mkdir(auto, { recursive: true });
-        await writeFile(path.join(auto, 'git-hash.func'), `const int git_hash = 0x${head.trim()};`);
+        await writeFile(path.join(auto, 'git-hash.tolk'), `const git_hash = 0x${head.trim()};`);
 
-        await writeFile(path.join(auto, 'metadata.func'),
+        await writeFile(path.join(auto, 'metadata.tolk'),
           `
-const slice NFT_URI = "${Metadata.NFT_URI}";
-const slice NFT_IMAGE_URI = "${Metadata.NFT_IMAGE_URI}";
+const NFT_URI = "${Metadata.NFT_URI}";
+const NFT_IMAGE_URI = "${Metadata.NFT_IMAGE_URI}";
           `
           );
     },
-    targets: ['contracts/versioning.func',
-              'contracts/pool.func'],
+    entrypoint: 'contracts/pool.tolk'
 };
