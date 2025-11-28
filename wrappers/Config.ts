a073e9545ce8df318a097039af48b7cb3bafaa5e
Trinketer22 <trinketer22@localhost>
2025-11-28 20:40:37 +0300

    Make config wrapper to return configuration cell

--- wrappers/Config.ts
+++ wrappers/Config.ts
@@ -1,4 +1,4 @@
-import { Address, Cell, Contract} from "@ton/core";
+import { Address, Cell, Contract, ContractProvider} from "@ton/core";
 
 export class Config implements Contract {
 	constructor(readonly address: Address,readonly init?: { code: Cell; data: Cell}){}
@@ -6,4 +6,15 @@ export class Config implements Contract {
 	static createFromAddress(address: Address) {
 		return new Config(address);
 	}
+    async getConfigCell(provider: ContractProvider) {
+        const state = await provider.getState();
+        if(state.state.type !== "active") {
+            throw new Error(`Config contract is ${state.state.type}`);
+        }
+        if(!state.state.data) {
+            throw new Error("Unable to retrieve config state data");
+        }
+        const dataCell = Cell.fromBoc(state.state.data)[0];
+        return dataCell.refs[0];
+    }
 }
